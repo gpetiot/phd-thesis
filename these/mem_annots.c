e7579667df8685e7b2000c5fa357c9b8f0ae8353
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-03-31 16:57:46 +0200

    C file to illustrate memory annots

---
+++ these/mem_annots.c
@@ -0,0 +1,15 @@
+extern void* malloc(unsigned long);
+extern void free(void*);
+
+void main() {
+  int * t = malloc(3*sizeof(int));
+  t[0] = 1;
+  t[1] = 1;
+  int * p = t+1;
+  //@ assert \base_addr(p) == (char*)t;
+  //@ assert \block_length(p) == 3 * sizeof(int);
+  //@ assert \offset(p) == 1*sizeof(int);
+  //@ assert \valid(p+(0..1));
+  //@ assert \initialized(p) && !\initialized(p+1);
+  free(t);
+}
