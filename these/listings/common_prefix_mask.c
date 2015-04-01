0e0cc0cb8f33d2c93fcf864485c6b430b1380849
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-04-01 11:02:25 +0200

    listings instead of figures

--- these/listings/common_prefix_mask.c
+++ these/listings/common_prefix_mask.c
@@ -4,8 +4,8 @@ byte  masks[] = {0x00,0x80,0xC0,0xE0,0xF0,0xF8,0xFC,0xFE,0xFF};
 int longer [] = {   0,  -1,   3,  -3,   6,  -5,   7,   8,  -8};
 int shorter[] = {   0,   0,   1,  -2,   2,  -4,   5,  -6,  -7};
 byte gtCommonPrefixMask(byte a, byte b) {
-  byte nxor = ~(a ^ b);   // a bit = 1 iff this bit is equal in a and b
-  int i = 4;              // search starts in the middle of the word
+  byte nxor = ~(a ^ b);   // a bit = 1 iff it is equal in a and b
+  int i = 4;              // search starts middle of the word
   while(i > 0)            // we stop when i<=0
     if (nxor >= masks[i]) // first i bits equal,
       i = longer[i];      // try a longer prefix 
