26c5508f17b21408cbd924bfe8d232d06ac0de06
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-05-05 16:17:08 +0200

    remarques JJ

--- these/listings/is_present_normalized.c
+++ these/listings/is_present_normalized.c
@@ -1,7 +1,7 @@
 /*@ requires 0 <= n;
     requires \valid(t+(0..n-1));
     typically n <= 6;
-    ensures \result != 0 <==> \exists integer i; 0<=i<\old(n)
+    ensures \result != 0 <==> \exists integer i; 0<=i<=\old(n)-1
                                 && \old(*(t+i))==\old(v); */
 int is_present(int* t, int n, int v) {
  §$Beg_{\texttt{is\_present}}:$§ int res = 0, i = 0;
@@ -11,7 +11,7 @@ int is_present(int* t, int n, int v) {
   while (i < n) {
     §$BegIter_{l_0}:$§
     if(*(t+i) == v) { res = 1; break; }
-    i++;
+    i = i+1;
     §$EndIter_{l_0}:$§ ;
   }
  §$End_{\texttt{is\_present}}:$§ return res;
