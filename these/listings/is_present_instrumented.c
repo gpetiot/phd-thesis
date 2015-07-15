c93be86ca7c8fe2a7263dfd087aba561b2cdf754
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-07-15 17:45:35 +0200

    remarques chapter_lang

--- these/listings/is_present_instrumented.c
+++ these/listings/is_present_instrumented.c
@@ -2,7 +2,7 @@ int is_present(int* t, int n, int v) {
  §$Beg_{\texttt{is\_present}}:$§
   int *old_t = t; int *old_val_t = malloc(((n-1)+1)*sizeof(int)); int old_n = n;
   int old_v = v; int res = 0; int i = 0; int iter_t;
-  for(iter_t = 0; iter_t < n; iter_t++) *(old_val_t+iter_t) = *(t+iter_t);
+  for(iter_t = 0; iter_t < n; iter_t++) old_val_t[iter_t] = t[iter_t];
   §$\underline{\Zinit \mbox{var\_{38} = 0}}$§; §$\underline{\Zinit \mbox{var\_{39} = n}}$§; int var_40 = §$\underline{ \mbox{var\_38}\Zclear\ \mbox{<= var\_{39}}\Zclear}$§; fassume(var_40);
   fassume(fvalidr(t,0,(n-1)));
   §$\underline{\Zinit \mbox{var\_{41} = n}}$§; §$\underline{\Zinit \mbox{var\_{42} = 6}}$§; int var_43 = §$\underline{ \mbox{var\_{}41}\Zclear\ \mbox{<= var\_{42}}\Zclear}$§; fassume(var_43);
@@ -10,7 +10,7 @@ int is_present(int* t, int n, int v) {
   §$\underline{\Zinit \mbox{var\_0 = 0}}$§; §$\underline{\Zinit \mbox{var\_1 = i}}$§; int var_2 = §$\underline{ \mbox{var\_0}\Zclear\ \mbox{<= var\_1}\Zclear}$§; int var_3 = var_2;
   if(var_3) { §$\underline{\Zinit \mbox{var\_4 = i}}$§; §$\underline{\Zinit \mbox{var\_5 = n}}$§; int var_6 = §$\underline{ \mbox{var\_4}\Zclear\ \mbox{<= var\_5}\Zclear}$§; var_3 = var_6; }
   fassert(var_3);
-  while(i < n && *(t+i) != v) {
+  while(i < n && t[i] != v) {
    §$BegIter_{l_0}:$§
     §$\underline{\Zinit \mbox{var\_7 = n}}$§; §$\underline{\Zinit \mbox{var\_8 = i}}$§; §$\underline{\Zinit \mbox{var\_9 = var\_7}\Zclear\ \mbox{- var\_8}\Zclear}$§;
     int var_10 = §$\underline{ \mbox{0 <= var\_9}}$§; fassert(var_10);
@@ -30,6 +30,6 @@ int is_present(int* t, int n, int v) {
   §$\underline{\Zinit \mbox{var\_{33} = 0}}$§; §$\underline{\Zinit \mbox{var\_{34} = old\_n}}$§;
   int var_35 = 0;
   for(§$\underline{\Zinit \mbox{i\_0 = var\_{33}}\Zclear}$§; §$\underline{ \mbox{i\_0 < var\_{34}}\Zclear}$§ && !var_35; §$\underline{ \mbox{i\_0++}\Zclear}$§) {
-    §$\underline{\Zinit \mbox{var\_{36} = i\_0}}$§; int var_37 = §$\underline{\mbox{var\_{36}}\Zclear}$§; var_35 = *(old_val_t + var_37) == old_value; }
+    §$\underline{\Zinit \mbox{var\_{36} = i\_0}}$§; int var_37 = §$\underline{\mbox{var\_{36}}\Zclear}$§; var_35 = old_val_t[var_37] == old_value; }
   fassert((!var_32 || var_35) && (!var_35 || var_32));
   free(old_val_t); return res; }
--- these/listings/is_present_normalized.c
+++ these/listings/is_present_normalized.c
@@ -2,13 +2,13 @@
              \valid(t+(0..n-1));
     typically n <= 6;
     ensures \result != 0 <==> \exists integer i; 0<=i<=\old(n)-1
-                                && \old(*(t+i))==\old(v); */
+                                && \old(t[i])==\old(v); */
 int is_present(int* t, int n, int v) {
   §$Beg_{\texttt{is\_present}}:$§ int res§$_{\texttt{is\_present}}$§ = 0; int i = 0;
  l§$_0$§: 
   /*@ loop invariant 0 <= i && i <= n;
       loop variant n - i; */
-  while (i < n && *(t+i) != v) {
+  while (i < n && t[i] != v) {
     §$BegIter_{l_0}:$§
     i = i+1;
     §$EndIter_{l_0}:$§ ;
