7a79ef5d4004d7ddde03f7d82155c3ac2f8cf54f
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-07-27 10:57:02 +0200

    ch_lang

--- these/listings/is_present_normalized.c
+++ these/listings/is_present_normalized.c
@@ -4,7 +4,7 @@
     ensures \result != 0 <==> \exists integer i; 0<=i<=\old(n)-1
                                 && \old(t[i])==\old(v); */
 int is_present(int* t, int n, int v) {
-  §$Beg_{\texttt{is\_present}}:$§ int res§$_{\texttt{is\_present}}$§ = 0; int i = 0;
+  §$Beg_{\texttt{is\_present}}:$§ int res§$_{\texttt{is\_present}}$§; int i; res§$_{\texttt{is\_present}}$§ = 0; i = 0;
  l§$_0$§: 
   /*@ loop invariant 0 <= i && i <= n;
       loop variant n - i; */
@@ -13,6 +13,6 @@ int is_present(int* t, int n, int v) {
     i = i+1;
     §$EndIter_{l_0}:$§ ;
   }
-  if(i < n) { res = 1; }
+  if(i < n) { res§$_{\texttt{is\_present}}$§ = 1; }
  §$End_{\texttt{is\_present}}:$§ return res§$_{\texttt{is\_present}}$§;
 }
